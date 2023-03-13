c275cb7f72d851a6409387c05539a4c20608f26e
Ousmane Barry <ppobarry@gmail.com>
2023-03-13 18:47:53 -0400

    server

--- server/main.js
+++ server/main.js
@@ -4,11 +4,12 @@ const connection = mysql.createConnection({
 	host: "sql9.freesqldatabase.com",
 	user: "sql9605441",
 	password: "Hs3TwsUyfl",
+	database: "sql9605441",
 });
 
 connection.connect();
 
-connection.query("SELECT * FROM sql9605441.Hotel_Chain", (err, res) => {
+connection.query("SELECT * FROM Hotel_Chain", (err, res) => {
 	if (err) console.log("There is an error: " + err);
 	console.log("The solution is: ", res);
 });
