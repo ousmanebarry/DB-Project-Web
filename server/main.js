60644207cd09c11734e3e5734bc3211db9f73e42
Ousmane Barry <ppobarry@gmail.com>
2023-03-13 18:42:37 -0400

    server

--- server/main.js
+++ server/main.js
@@ -1,16 +1,16 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'Ottawa@2023',
+	host: "sql9.freesqldatabase.com",
+	user: "sql9605441",
+	password: "Hs3TwsUyfl",
 });
 
 connection.connect();
 
-connection.query('SELECT * FROM hotel_management.Hotel_Chain', (err, res) => {
-	if (err) console.log('There is an error: ' + err);
-	console.log('The solution is: ', res);
+connection.query("SELECT * FROM sql9605441.Hotel_Chain", (err, res) => {
+	if (err) console.log("There is an error: " + err);
+	console.log("The solution is: ", res);
 });
 
 connection.end();
