c34e012626e1ee9e3a89fb70d5db443be75342fa
ousmanebarry <ppobarry@gmail.com>
2023-03-14 01:12:05 -0400

    server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,13 @@
 import { db } from "../db.js";
 
-export const elogin = (req, res) => {};
+export const elogin = (req, res) => {
+	let q = "CALL Hotel_Chain_Test()";
+
+	db.query(q, (err, data) => {
+		if (err) return res.json(err);
+		console.log(data);
+	});
+};
 
 export const clogin = (req, res) => {};
 
