524728783aae5125a2e06d96ea86ecb06d7a510a
ousmanebarry <ppobarry@gmail.com>
2023-03-14 01:34:25 -0400

    server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,22 @@
 import { db } from "../db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const elogin = (req, res) => {
-	let q = "CALL Employee_Login()";
+	let q = "CALL Employee_Login(?)";
 
-	db.query(q, (err, data) => {
+	db.query(q, req.body.username, (err, data) => {
 		if (err) return res.json(err);
+		if (data.length == 0) return res.status(404).json("User not found");
+
+		const isPasswordCorrect = bcrypt.compareSync(
+			req.body.passwod,
+			data[0].password
+		);
+
+		if (!isPasswordCorrect)
+			return res.status(400).json("Wrong username or password");
+
 		console.log(data);
 	});
 };
