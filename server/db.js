1a9b245a14474b014687083d8440beae6599a231
Ousmane Barry <ppobarry@gmail.com>
2023-03-22 20:07:22 -0400

    use local db connection

---
+++ server/db.js
@@ -0,0 +1,24 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+	host: process.env.host,
+	user: process.env.user,
+	password: process.env.password,
+	database: process.env.database,
+});
+
+connection.query("SELECT * FROM hotel_chain", (err, res) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	console.log(res);
+});
+
+connection.end();
+
+export default connection;
