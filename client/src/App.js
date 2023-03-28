9d697fe3d7e2b6c21cf6dc51ce9416f163d9f18a
ousmanebarry <ppobarry@gmail.com>
2023-03-28 10:39:42 -0400

    rename routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
 import { useState } from "react";
 import Booking from "./components/CustomerView/Booking.js";
 import { UserContext } from "./Contexts/UserContext";
@@ -10,65 +15,64 @@ import HomePage from "./components/Home/home";
 import Branches from "./components/HotelBranches/hotelBranches";
 
 function App() {
+	const [user, setUser] = useState({
+		id: 1,
+		firstName: "",
+		lastName: "",
+		position: "",
+		authenticated: false,
+	});
 
-  const [user, setUser] = useState({
-    id: 1,
-    firstName: "",
-    lastName: "",
-    position: "",
-    authenticated: false,
-  });
+	return (
+		<Router>
+			<UserContext.Provider value={{ user, setUser }}>
+				<Switch>
+					<Route exact path="/">
+						<Navbar
+							List={[
+								{ display: "Sign In", path: "/login" },
+								{ display: "Branches", path: "/branches" },
+							]}
+						/>
+						<HomePage />
+						<Footer />
+					</Route>
 
-  return (
-    <Router>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Switch>
-          <Route exact path="/Database">
-            <Navbar
-              List={[
-                { display: "Sign In", path: "/Database/signin" },
-                { display: "Branches", path: "/Database/branches" },
-              ]}
-            />
-            <HomePage />
-            <Footer />
-          </Route>
+					<Route exact path="/login">
+						<Navbar List={[{ display: "Home Page", path: "/Database" }]} />
+						<SignIn />
+						<Footer />
+					</Route>
 
-          <Route exact path="/Database/signin">
-            <Navbar List={[{ display: "Home Page", path: "/Database" }]} />
-            <SignIn />
-            <Footer />
-          </Route>
+					<Route exact path="/booking">
+						<Navbar
+							List={[
+								{ display: "Home Page", path: "/" },
+								{ display: "Branches", path: "/branches" },
+							]}
+						/>
+						<Booking />
+						<Footer />
+					</Route>
 
-          <Route exact path="/Database/Booking">
-            <Navbar
-              List={[
-                { display: "Home Page", path: "/Database/" },
-                { display: "Branches", path: "/Database/branches" },
-              ]}
-            />
-            <Booking />
-            <Footer />
-          </Route>
+					<Route exact path="/branches">
+						<Navbar
+							List={[
+								{ display: "Home Page", path: "/" },
+								{ display: "Booking", path: "/booking" },
+							]}
+						/>
+						<Branches />
+						<Footer />
+					</Route>
 
-          <Route exact path="/Database/branches">
-            <Navbar
-              List={[
-                { display: "Home Page", path: "/Database/" },
-                { display: "Booking", path: "/Database/Booking" },
-              ]}
-            />
-            <Branches />
-            <Footer />
-          </Route>
-
-          <Route exact path="/*">
-            <Redirect to={"/Database/"} />
-          </Route>
-        </Switch>
-      </UserContext.Provider>
-    </Router>
-  );
+					<Route exact path="/*">
+						<Redirect to={"/"} />
+					</Route>
+				</Switch>
+			</UserContext.Provider>
+		</Router>
+	);
 }
 
 export default App;
