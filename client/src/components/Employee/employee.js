04e81e556a1843369717d40ecebb1c0bdd8f2cee
ousmanebarry <ppobarry@gmail.com>
2023-03-31 20:23:20 -0400

    fix errors

--- client/src/components/Employee/employee.js
+++ client/src/components/Employee/employee.js
@@ -1,9 +1,10 @@
 import "./employee.css";
 import React, { useState, useEffect } from "react";
+import { Row, Col, Form, Card, Button, Modal } from "react-bootstrap";
 
 function Employee() {
 	const [bookings, setBookings] = useState([]);
-	
+
 	useEffect(() => {
 		fetchRooms();
 	}, []);
@@ -12,7 +13,6 @@ function Employee() {
 		const requestOptions = {
 			method: "GET",
 			headers: { "Content-Type": "application/json" },
-			
 		};
 
 		fetch("http://localhost:8800/api/bookings", requestOptions)
@@ -23,31 +23,28 @@ function Employee() {
 	return (
 		<div>
 			{bookings.map((b, index) => {
-				return <Col md={6} lg={4} className="mb-4">
-				<Card>
-					<Card.Body>
-						<Card.Title>
-							<b>{`${b.Full_name}`}</b>
-						</Card.Title>
-						<Card.Text>
-						<b>{`${b.Chain_Name} ${b.Category} Room #${b.Room_Number}`}</b>
-							<ul key={h.roomId}>
-								<li>First_Days: {b.First_Days}</li>
-								<li>Last_Day: {b.Last_Day}</li>
-								<li>Price: {`$${b.Price}`}</li>
-							</ul>
-						</Card.Text>
-						<Button
-							variant="primary"
-							onClick={() => handleShow(index)}
-						>
-							More Info
-						</Button>
-					</Card.Body>
-				</Card>
-			</Col>
+				return (
+					<Col md={6} lg={4} className="mb-4">
+						<Card>
+							<Card.Body>
+								<Card.Title>
+									<b>{`${b.Full_name}`}</b>
+								</Card.Title>
+								<Card.Text>
+									<b>{`${b.Chain_Name} ${b.Category} Room #${b.Room_Number}`}</b>
+									<ul key={b.roomId}>
+										<li>First_Days: {b.First_Day}</li>
+										<li>Last_Day: {b.Last_Day}</li>
+										<li>Price: {`$${b.Price}`}</li>
+									</ul>
+								</Card.Text>
+								<Button variant="primary">More Info</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+				);
 			})}
-         </div>
+		</div>
 	);
 }
 
