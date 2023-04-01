4f4c30075be859c41e980cdd48e19ac88750ccc7
kr134 <krizk083@uottawa.ca>
2023-03-31 20:18:09 -0400

    Update employee.js

--- client/src/components/Employee/employee.js
+++ client/src/components/Employee/employee.js
@@ -1,11 +1,52 @@
-import React from "react";
 import "./employee.css";
+import React, { useState, useEffect } from "react";
 
 function Employee() {
+	const [bookings, setBookings] = useState([]);
+	
+	useEffect(() => {
+		fetchRooms();
+	}, []);
+
+	const fetchRooms = () => {
+		const requestOptions = {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+			
+		};
+
+		fetch("http://localhost:8800/api/bookings", requestOptions)
+			.then((response) => response.json())
+			.then((data) => setBookings(data));
+	};
+
 	return (
 		<div>
-        
-        
+			{bookings.map((b, index) => {
+				return <Col md={6} lg={4} className="mb-4">
+				<Card>
+					<Card.Body>
+						<Card.Title>
+							<b>{`${b.Full_name}`}</b>
+						</Card.Title>
+						<Card.Text>
+						<b>{`${b.Chain_Name} ${b.Category} Room #${b.Room_Number}`}</b>
+							<ul key={h.roomId}>
+								<li>First_Days: {b.First_Days}</li>
+								<li>Last_Day: {b.Last_Day}</li>
+								<li>Price: {`$${b.Price}`}</li>
+							</ul>
+						</Card.Text>
+						<Button
+							variant="primary"
+							onClick={() => handleShow(index)}
+						>
+							More Info
+						</Button>
+					</Card.Body>
+				</Card>
+			</Col>
+			})}
          </div>
 	);
 }
